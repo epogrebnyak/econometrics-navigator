5fb706c77eaa805ac669d60972a2fb80eb53ec71
epogrebnyak <e.pogrebnyak@gmail.com>
2018-11-16 13:54:21 +0300

    .1 version added

--- conf.py
+++ conf.py
@@ -62,7 +62,7 @@ author = 'Evgeny Pogrebnyak'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0'
+version = '0.0.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.0.1'
 
